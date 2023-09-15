49f0798c6f52c9332400895ae6deb36d9230609e
GarudaOne <2074857+garudaonekh@users.noreply.github.com>
2023-09-15 21:47:31 +0700

    Add files via upload

---
+++ src/internal/ConnectionClient.h
@@ -0,0 +1,26 @@
+#ifndef __CONNECTION_CLIENT_H__
+#define __CONNECTION_CLIENT_H__
+
+#include <WiFi.h>
+#include <Arduino.h>
+
+typedef WiFiClient ConnectionClient;
+
+/*#ifdef ARDUINO_SAMD_ZERO
+
+#include <WiFi101.h>
+typedef WiFiClient ConnectionClient;
+
+#elif ARDUINO_AVR_YUN
+
+#include <Bridge.h>
+typedef Process ConnectionClient;
+
+#elif ARDUINO_ARCH_ESP8266
+
+#include <WiFiClientSecure.h>
+typedef WiFiClientSecure ConnectionClient;
+
+#endif*/
+
+#endif //__CONNECTION_CLIENT_H__
